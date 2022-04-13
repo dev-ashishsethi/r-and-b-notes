2e3d3bc67377dd28d6ed71db56c35f0163ca6d99
dev-ashishsethi <me.ashishsethi@gmail.com>
2022-04-13 13:20:43 +0530

    setup

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-
+import { BrowserRouter, BrowserRouter as Router } from "react-router-dom";
 // Call make Server
 makeServer();
 
